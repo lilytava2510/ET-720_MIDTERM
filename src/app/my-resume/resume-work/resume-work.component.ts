8010058d47e3071d7463865703dcd5f62eb977f6
lilytava2510 <lilytavares10@gmail.com>
2020-10-16 12:56:40 -0400

    resume done

---
+++ src/app/my-resume/resume-work/resume-work.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-work',
+  templateUrl: './resume-work.component.html',
+  styleUrls: ['./resume-work.component.css']
+})
+export class ResumeWorkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
